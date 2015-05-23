427d61f27ec23030efeae4eb95dee4364ebc751f
金森幹斗 <mikito1188@icloud.com>
2015-05-23 15:43:44 +0900

    ターミナルからやってみました！

--- 10seconds/ViewController.swift
+++ 10seconds/ViewController.swift
@@ -9,15 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var num:Double = 0
+    
+    @IBOutlet var label:UILabel!
+    
+    var timer:NSTimer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = String(format: "%.20f", Float(num))
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tapStart(){
+
+            
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("countup"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func tapStop(){
+        
+        timer.invalidate()
+        
+    }
+    
+    func countup(){
+        
+        num += 0.01
+        label.text = String(format: "%.20f", Float(num))
+        //NSLog("%.20f",num)
+        
     }
 
 
